844727812b6d9d6da6d8bf410693f0dca212e787
VillePajala <valoraami@gmail.com>
2019-02-20 15:36:41 +0200

    Changes to heroku preferences

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
+app.use(express.static(__dirname + '/dist/portfolio'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+res.sendFile(path.join(__dirname+'/dist/portfolio/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
