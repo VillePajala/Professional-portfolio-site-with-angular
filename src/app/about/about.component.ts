06aa57f128fb7b612b3a804470dba5319042c7d0
VillePajala <valoraami@gmail.com>
2019-03-09 15:41:25 +0200

    Added instagram Api service

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,3 +1,4 @@
+import { InstagramService } from './../instagram.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  constructor(private instagram : InstagramService) { }
 
   ngOnInit() {
   }
---
+++ src/app/instagram.service.ts
@@ -0,0 +1,30 @@
+import { ApiService } from './api.service';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InstagramService {
+
+  apiKey : string;
+
+  constructor(private http : HttpClient, private token : ApiService) {
+    
+   }
+
+  information : object = [];  
+  private apiUrl : string = `https://api.instagram.com/v1/users/self/media/recent/?access_token=${this.token.apiKey}`;
+
+  getFeed = () : any => {
+
+    this.http.get(this.apiUrl).subscribe((data : any) => {
+      this.information = data.data;
+
+    },
+    (err : any) => {
+      console.log(err);
+    });
+  }
+  
+}
