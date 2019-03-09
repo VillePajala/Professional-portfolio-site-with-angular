c4a80aa24976277986ca01f5ee15040384772309
VillePajala <valoraami@gmail.com>
2019-03-09 21:44:13 +0200

    Update instagram.service.ts

--- src/app/instagram.service.ts
+++ src/app/instagram.service.ts
@@ -1,4 +1,4 @@
-import { ApiService } from './api.service';
+/* import { ApiService } from './api.service'; */
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
