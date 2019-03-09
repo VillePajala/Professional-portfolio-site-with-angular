0cdb7acc015c28459d09f5b5a6ec56f7f578895d
VillePajala <valoraami@gmail.com>
2019-03-09 22:56:48 +0200

    Update about.component.ts

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  constructor(private instagram : InstagramService) { }
+  constructor(public instagram : InstagramService) { }
 
   ngOnInit() {
   }
